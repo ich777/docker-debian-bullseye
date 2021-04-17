6a429e8229b7249821c9e92c4a9aa37f560e189b
ich777 <ich777@noreply.example.org>
2021-04-17 08:07:18 +0200

    „scripts/start.sh“ ändern

--- scripts/start.sh
+++ scripts/start.sh
@@ -51,7 +51,8 @@ if [ ! -d /var/run/dbus ]; then
 fi
 chown -R ${UID}:${GID} /var/run/dbus/
 chmod -R 770 /var/run/dbus/
-chown -R ${UID}:${GID} /opt/scripts
+chown -R root:${GID} /opt/scripts
+chmod -R 750 /opt/scripts
 chown -R ${UID}:${GID} /tmp/xdg
 chmod -R 0700 /tmp/xdg
 dbus-uuidgen > /var/lib/dbus/machine-id
